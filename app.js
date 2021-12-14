642b9b432143a1c876fdaf44cbf9be6bc239a519
Ankush Dua <ankushduacodes@gmail.com>
2021-12-14 21:50:39 +0530

    prod prep

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ import methodOverride from 'method-override';
 // eslint-disable-next-line import/extensions
 import defRouter from './routes/index.js';
 // eslint-disable-next-line import/extensions
-import config from './config/config.js';
+// import config from './config/config.js';
 // eslint-disable-next-line import/extensions
 import authenticate from './config/passport.js';
 
 mongoose.connect(
-  `mongodb+srv://${process.env.USERNAME || config.USERNAME}:${process.env.PASSWORD || config.PASSWORD}@cluster0.rd7qo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.rd7qo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true },
 // eslint-disable-next-line no-console
 ).then(() => console.log('connected')).catch((e) => console.log('connection failure\n', e));
@@ -31,7 +31,7 @@ app.use(express.json());
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(passport.initialize());
 app.use(session({
-  secret: process.env.SECRET || config.SECRET,
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false,
   store: session.sessionStore,
